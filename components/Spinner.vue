67f81151358f9f12caa78c8656860f29f160a0b0
VihangaDehigaspitiya <vihanga.2016417@iit.ac.lk>
2020-08-10 14:49:30 +0530

    policy json added loader added

---
+++ components/Spinner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "spinner"
+    }
+</script>
+
+<style scoped>
+
+</style>
