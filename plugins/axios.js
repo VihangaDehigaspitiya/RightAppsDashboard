3a99c5159b83ff7bc28a2eb4959f76753ab791d9
VihangaDehigaspitiya <vihanga.2016417@iit.ac.lk>
2020-08-10 13:56:34 +0530

    cors url updated

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,15 +1,19 @@
-import {setAuthToken, resetAuthToken} from '../common/utils/auth';
-import axios from 'axios';
+import { setAuthToken, resetAuthToken } from "../common/utils/auth";
+import axios from "axios";
 import JwtService from "../common/jwt.service";
 
 const token = JwtService.getToken();
 
-if (process.server) axios.defaults.baseURL = 'http://test.rightapps.net.au:3000/';
-else axios.defaults.baseURL = 'http://test.rightapps.net.au:3000/';
+if (process.server)
+  axios.defaults.baseURL =
+    "https://cors-anywhere.herokuapp.com/http://test.rightapps.net.au:3000/";
+else
+  axios.defaults.baseURL =
+    "https://cors-anywhere.herokuapp.com/http://test.rightapps.net.au:3000/";
 
 if (token) {
-  console.log("Token", token)
-  setAuthToken(token)
+  console.log("Token", token);
+  setAuthToken(token);
 } else {
-  resetAuthToken()
+  resetAuthToken();
 }
